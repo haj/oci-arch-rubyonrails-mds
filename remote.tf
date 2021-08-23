cf08f459dde2c7e62f4bb5918abf2120cb8cb0b3
Hassan Ajan <hassanajan@gmail.com>
2021-08-24 01:22:36 +0200

    fix bastion ssh bastion user

--- remote.tf
+++ remote.tf
@@ -38,10 +38,10 @@ resource "null_resource" "ror_bootstrap" {
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
-      timeout             = "10m"
+      timeout             = "1m"
       bastion_host        = var.use_bastion_service ? "host.bastion.${var.region}.oci.oraclecloud.com" : oci_core_instance.bastion_instance[0].public_ip
       bastion_port        = "22"
-      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "opc"
+      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "ubuntu"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
     }
 
@@ -58,10 +58,10 @@ resource "null_resource" "ror_bootstrap" {
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
-      timeout             = "10m"
+      timeout             = "1m"
       bastion_host        = var.use_bastion_service ? "host.bastion.${var.region}.oci.oraclecloud.com" : oci_core_instance.bastion_instance[0].public_ip
       bastion_port        = "22"
-      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "opc"
+      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "ubuntu"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
     }
 
@@ -76,10 +76,10 @@ resource "null_resource" "ror_bootstrap" {
       private_key         = tls_private_key.public_private_key_pair.private_key_pem
       script_path         = "/home/opc/myssh.sh"
       agent               = false
-      timeout             = "10m"
+      timeout             = "15m"
       bastion_host        = var.use_bastion_service ? "host.bastion.${var.region}.oci.oraclecloud.com" : oci_core_instance.bastion_instance[0].public_ip
       bastion_port        = "22"
-      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "opc"
+      bastion_user        = var.use_bastion_service ? oci_bastion_session.ssh_via_bastion_service[count.index].id : "ubuntu"
       bastion_private_key = tls_private_key.public_private_key_pair.private_key_pem
 
     }
